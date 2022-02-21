1ac36b93540b35aaa9bb95dbf7e8da7c4e3b9cd1
Max Brehmer <mxbrhmr@gmail.com>
2022-02-21 10:48:39 -0600

    Added home screen overlay, start function, and text vals

--- app/src/main/java/com/example/team12_hw1/MainActivity.kt
+++ app/src/main/java/com/example/team12_hw1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.team12_hw1
 import adapter.ItemAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import data.Datasource
 
@@ -14,5 +16,10 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
         recyclerView.setHasFixedSize(true)
+    }
+
+    fun start(view: View) {
+        val welcome: LinearLayout = findViewById(R.id.welcome_mod)
+        welcome.setVisibility(View.GONE)
     }
 }
